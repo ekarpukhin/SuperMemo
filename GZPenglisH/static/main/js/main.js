e9aeffe5915a651a5cf78e68666c6cd472e770db
Konstantin <61847416+KostyaRabBozhiy@users.noreply.github.com>
2022-08-01 10:53:26 +0300

    Some

--- GZPenglisH/static/main/js/main.js
+++ GZPenglisH/static/main/js/main.js
@@ -1,37 +1,53 @@
-function getAnswer(){
-  let form = document.getElementById("input-form");
+function getAnswer() {
+    let form = document.getElementById("input-form");
 
-  function complete(value) {
-    document.onkeydown = null;
-    window.alert( value );
-  }
+    function complete(value) {
+        document.onkeydown = null;
+        window.alert(value);
+    }
 
-  form.onsubmit = function (){
-    let value = form.text.value;
-    if (value == '') return false;
+    form.onsubmit = function () {
+        let value = form.text.value;
+        if (value == '') return false;
+        $("document").ready(function () {
+            $.ajax({
+                url: "/CardModal.js",
+                //type: "POST",
+                method: "post",
+                dataType: "json",
+                data: {
+                    url: value,
+                    csrfmiddlewaretoken: '{{ csrf_token }}'
+                },
+                error: function (xhr, errmsg, err) {
+                    window.alert("Sending to Django: something went wrong!\n" + xhr.status + ": " + xhr.responseText);
+                }
+            });
+        });
 
-    complete(value);
-  };
+        complete(value);
+        return false;
+    };
 
-  return form.text.value;
+    return form.text.value;
 }
 
 
 let modalWrap = null;
 
 const showModal = arguments => {
-  if (modalWrap !== null) {
-    modalWrap.remove();
-  }
+    if (modalWrap !== null) {
+        modalWrap.remove();
+    }
 
-  let str_out = arguments;
-  if (typeof arguments !== "string") {
-    str_out = arguments[0]
-  }
-  let str_in = null;
+    let str_out = arguments;
+    if (typeof arguments !== "string") {
+        str_out = arguments[0]
+    }
+    let str_in = null;
 
-  modalWrap = document.createElement('div');
-  modalWrap.innerHTML = `
+    modalWrap = document.createElement('div');
+    modalWrap.innerHTML = `
     <div class="modal fade" id="CardModal">
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
@@ -55,27 +71,15 @@ const showModal = arguments => {
   `;
 
 
-  modalWrap.querySelector('.modal-success-btn').onclick = function (){
-    str_in = getAnswer();
-//"/courses/views/urls/get_answer_from_js",
-    $.ajax({
-    url: "/courses/templates/courses/someshit.py",
-    type: "POST",
-    dataType: "json",
-    data: {
-      url: str_in,
-      csrfmiddlewaretoken: '{{ csrf_token }}'
-    },
-    error: function (xhr, errmsg, err) {
-      window.alert("Sending to Django: something went wrong!\n" + xhr.status + ": " + xhr.responseText);
-    }
-  });
-  };
+    modalWrap.querySelector('.modal-success-btn').onclick = function () {
+        str_in = getAnswer();
+        //          /courses/templates/courses/someshit.py
+    };
 
-  document.body.append(modalWrap);
+    document.body.append(modalWrap);
 
-  let modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
-  modal.show();
+    let modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
+    modal.show();
 }
 
 
