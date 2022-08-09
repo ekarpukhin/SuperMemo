d7efcd9f4644c983bb623626876c82d8f9ef0ed0
IPupyshev <pupyshev2212@gmail.com>
2022-08-09 20:38:28 +0300

    Finally ajax

--- GZPenglisH/courses/views.py
+++ GZPenglisH/courses/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+
 
 
 class CourseViewMain(View):
@@ -12,7 +14,9 @@ class CourseViewMain(View):
         }
         return render(request, 'courses/courses_page.html', data)
 
-
+@csrf_exempt
 def get_answer_form_js(request):
     answer = request.POST.get('url')
+    print("Yes")
     print("Your answer: {}".format(answer))
+    return JsonResponse({'status': 'Todo added!'})
---
+++ GZPenglisH/static/main/js/ShowCardModal.js
@@ -0,0 +1,106 @@
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === (name + "=")) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
+function getAnswer() {
+  let form = document.getElementById("input-form");
+  let text_out = {
+      value: form.text.value
+  };
+
+  function complete(value) {
+      document.onekeydown = null;
+      form.text.value = '';
+  }
+
+  form.onsubmit = function () {
+      let value = form.text.value;
+      if (value == '') return false;
+
+
+
+      $.ajax({
+          url: '/courses/get_answer/',
+          method: "POST",
+          dataType: "json",
+          headers: {
+            "X-Requested-With": "XMLHttpRequest",
+            "X-CSRFToken": getCookie("csrftoken"),  // don't forget to include the 'getCookie' function
+          },
+          data: {
+              url: value
+          },
+      }).done(function (xhr) {
+              window.alert("All is good, you're doing fine! status:" + xhr.status+ "\nResponseText\n" + xhr.responseText);
+          }
+      ).fail(function (xhr, errmsg, err) {
+          window.alert("Sending to PHP: something went wrong!\n" + xhr.status + ": " + xhr.responseText);
+      });
+
+      complete(value);
+      return false;
+  };
+
+  return text_out.value;
+}
+
+
+let modalWrap = null;
+
+const showModalCard = data => {
+    if (modalWrap !== null) {
+        modalWrap.remove();
+    }
+
+    let str_out = data;
+    let str_in = null;
+    if (typeof data !== "string") {
+        str_out = data[0]
+    }
+
+
+    modalWrap = document.createElement('div');
+    modalWrap.innerHTML = `
+    <div class="modal fade" id="CardModal">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header bg-light">
+            <h5 class="modal-title">Напиши перевод слова</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <form id="input-form">
+          <div class="modal-body">
+            <p>` + str_out + `</p>
+            <input name="text" type="text" size="50">
+          </div>
+          <div class="modal-footer justify-content-center">
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Закончить!</button>
+            <input type="submit" class="btn btn-success modal-success-btn" value="Ответить!">
+          </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  `;
+
+    modalWrap.querySelector('.modal-success-btn').onclick = function () {
+        str_in = getAnswer();
+    };
+
+    document.body.append(modalWrap);
+
+    let modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
+    modal.show();
+}
