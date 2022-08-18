742365b54cb318626e3d6f602ecb5a462458128e
Konstantin <61847416+Kostyak7@users.noreply.github.com>
2022-08-18 16:20:35 +0300

    global update

---
+++ main/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+
+class MainViewMain(View):
+
+    def get(self, request, *args, **kwargs):
+        data = {
+            "is_login": False,
+        }
+        return render(request, 'main/main_page.html')
+
+
+class AboutViewMain(View):
+
+    def get(self, request, *args, **kwargs):
+        data = {
+            "is_login": False,
+        }
+        return render(request, 'main/about_info.html')
---
+++ static/main/js/ShowCardModal.js
@@ -0,0 +1,103 @@
+let gradenote = {
+    0: "Dolboeb, incorrect",
+    1: "Very Bad",
+    2: "Very Bad, A few letters are correct",
+    3: "Mediocre",
+    4: "A few letters incorrect",
+    5: "Excellent!"
+}
+let modalWrap = null;
+let ability_to_answer = true;
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+        const cookies = document.cookie.split(";");
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + "=")) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function getAnswer() {
+    let form = document.getElementById("input-form");
+    let card_modal = document.getElementById("CardModal");
+    let text_out = {
+        value: form.text.value
+    };
+    let json_return = null;
+
+    function complete(value) {
+        document.onekeydown = null;
+        form.text.value = '';   // Что бы была пустая строка после ввода
+    }
+
+    const changeQuestion = (new_word) => {
+        let field = document.getElementById("question-field");
+        field.textContent = new_word;
+    }
+
+    const endingOfStudy = () => {
+        document.getElementById("btn-answer").remove();
+        document.getElementById("answer-field").remove();
+        ability_to_answer = false;
+    }
+
+    form.onsubmit = function () {
+        let value = form.text.value;
+        if (value == '') return false;
+
+        $.ajax({
+            url: '/courses/get_answer/',
+            method: "POST",
+            dataType: "json",
+            headers: {
+                "X-Requested-With": "XMLHttpRequest",
+                "X-CSRFToken": getCookie("csrftoken"),  // don't forget to include the 'getCookie' function
+            },
+            data: {
+                url: value
+            },
+        }).done(function (xhr) {
+                //window.alert("All is good, you're doing fine! status:" + xhr.status + "\nResponseText\n" + xhr.responseText);
+                window.alert(gradenote[xhr.answer_status]);
+                changeQuestion(xhr.new_word);
+                if (xhr.end_of_study) endingOfStudy();
+                json_return = xhr;
+            }
+        ).fail(function (xhr, errmsg, err) {
+            //window.alert("Sending to Py: something went wrong!\n" + xhr.status + ": " + xhr.responseText);
+        });
+
+        complete(value);
+        return false;
+    };
+
+
+    return json_return;
+}
+
+function showModalCard() {
+    let xhr_return = null;
+    modalWrap = document.getElementById('OutCardModal');
+    if (ability_to_answer) {
+        modalWrap.querySelector('.modal-success-btn').onclick = function () {
+            xhr_return = getAnswer();
+        };
+    }
+
+    let modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
+    modal.show();
+}
+
+function showModalOverCard() {
+    modalWrap = document.getElementById('OutCardModal');
+    let modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
+    modal.show();
+}
